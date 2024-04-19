a51ad55f33a17faf383583ad3656da54a6a1f96e
Prathap Chandra <prathap.chandra.754@gmail.com>
2024-04-19 21:40:30 +0530

    initialized the repo

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended', // Add this line
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'prettier'], // Add 'prettier' here
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'prettier/prettier': 'error', // Ensure Prettier errors are visible
+    },
+}
